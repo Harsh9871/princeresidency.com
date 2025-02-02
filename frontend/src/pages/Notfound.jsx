7172fc4f3618b9bbcd3324dd9de71c8a52d98bad
Harsh9871 <raithathaproduction@gmail.com>
2025-02-02 18:40:38 +0530

    add frontend files

---
+++ frontend/src/pages/Notfound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '../components/ui/button.jsx'
+const Notfound = () => {
+    return (
+        <div>
+            404
+            <Button>Click me</Button>
+        </div>
+    )
+}
+
+export default Notfound
